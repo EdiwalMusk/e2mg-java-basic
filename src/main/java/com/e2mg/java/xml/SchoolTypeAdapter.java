0abbad9eb7138e1913b8c199d0f84e3e5641c41d
zhangchenchen <1520406629@qq.com>
2024-08-21 21:30:17 +0800

    xml动态读取

--- src/main/java/com/e2mg/java/xml/SchoolTypeAdapter.java
+++ src/main/java/com/e2mg/java/xml/SchoolTypeAdapter.java
@@ -1,19 +1,24 @@
 package com.e2mg.java.xml;
 
 import com.alibaba.fastjson.JSONObject;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
-import com.sun.org.apache.xerces.internal.dom.ElementNSImpl;
+import org.w3c.dom.Node;
 
 import javax.xml.bind.annotation.adapters.XmlAdapter;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
 
 public class SchoolTypeAdapter extends XmlAdapter<Object, JSONObject> {
 
     @Override
     public JSONObject unmarshal(Object v) throws Exception {
-        ElementNSImpl ns = (ElementNSImpl) v;
-        String vv = new ObjectMapper().writeValueAsString(ns);
-        return new XmlMapper().readValue(vv, JSONObject.class);
+        Transformer transformer = TransformerFactory.newInstance().newTransformer();
+        StreamResult result = new StreamResult(new StringWriter());
+        transformer.transform(new DOMSource((Node) v), result);
+        return new XmlMapper().readValue(result.getWriter().toString(), JSONObject.class);
     }
 
     @Override
